6287f235b8d1c1ec582be8251e3bd6641e899d72
raffa50 <reloader90@gmail.com>
2017-04-21 19:50:00 +0200

    added gpsmap

--- FdsApp/FdsApp/MainPage.xaml.cs
+++ FdsApp/FdsApp/MainPage.xaml.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.Geolocator;
+using Xamarin.Forms.Maps;
+using Plugin.Geolocator.Abstractions;
 
-namespace FdsApp
-{
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
+namespace FdsApp{
+	public partial class MainPage : ContentPage	{
+		public MainPage() {
 			InitializeComponent();
-		}
-	}
+
+            CrossGeolocator.Current.PositionChanged += _geoUpdate;
+        }
+
+        private void _geoUpdate(object sender, PositionEventArgs e) {
+            MyMap.MoveToRegion( MapSpan.FromCenterAndRadius( 
+                new Xamarin.Forms.Maps.Position(e.Position.Latitude, e.Position.Longitude), 
+                Distance.FromMiles(1))
+            );
+        }
+    }
 }
